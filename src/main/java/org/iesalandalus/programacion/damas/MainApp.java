c3c88fbdb00cc62793091485ed888960ec09bdf8
Rafa <rafael.segura.orta@gmail.com>
2024-11-10 13:55:51 +0100

    Creamos la clase Direccion y almacenamos enumerados

---
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.damas;
+
+public class MainApp {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public enum Direccion {
+
+    NORESTE("Noreste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste"),
+    NOROESTE("Noroeste");
+
+
+}
