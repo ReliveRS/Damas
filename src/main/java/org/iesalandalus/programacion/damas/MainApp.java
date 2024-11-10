7af03b4d68099b2d9e9d7a331b80759862530b5a
Rafa <rafael.segura.orta@gmail.com>
2024-11-10 16:37:43 +0100

    Creamos metodo ejecutarOpcion

--- src/main/java/org/iesalandalus/programacion/damas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -10,7 +10,25 @@ public class MainApp
     public static void main(String[] args){
 
     }
-
+    // Método para ejecutar una opción del menú
+    public static void ejecutarOpcion(int opcion) {
+        switch (opcion) {
+            case 1:
+                crearDamaDefecto();  // Crear dama por defecto
+                break;
+            case 2:
+                crearDamaColor();  // Crear dama con color elegido
+                break;
+            case 3:
+                mover();  // Mover la dama
+                break;
+            case 4:
+                Consola.despedirse();  // Despedida y salir
+                break;
+            default:
+                System.out.println("Opción no válida.");
+        }
+    }
 
 
 
