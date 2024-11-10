67f3680157e426969e4d0d991a9c74d4e61f4509
Rafa <rafael.segura.orta@gmail.com>
2024-11-10 14:08:46 +0100

    Creamos clase Posicion y los atributos fila y columna

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -0,0 +1,18 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public class Posicion {
+
+    private int fila;  // Fila (de 1 a 8)
+    private char columna;  // Columna (de 'a' a 'h')
+
+
+
+
+
+
+
+
+
+
+
+}
