46e8c8ef5c711a8c4c9024bb528c0df0c2685ca0
Rafa <rafael.segura.orta@gmail.com>
2024-11-10 14:47:59 +0100

    Creamos Clase consola

---
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -0,0 +1,15 @@
+package org.iesalandalus.programacion.damas;
+
+public class Consola {
+
+    private Consola(){
+        throw new UnsupportedOperationException("Esta clase no puede ser instanciada.");
+    }
+
+
+
+
+
+
+
+}
