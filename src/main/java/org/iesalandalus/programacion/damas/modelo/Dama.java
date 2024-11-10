9246ce144f0f2beb360d64a42c500cbf25750ff7
Rafa <rafael.segura.orta@gmail.com>
2024-11-10 14:29:28 +0100

    Creamos clase Dama y sus atributos

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -0,0 +1,37 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public class Dama {
+
+//Atributos de la Clase Dama
+
+    private Color color;
+    private Posicion posicion;
+    private static boolean esDamaEspecial;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
