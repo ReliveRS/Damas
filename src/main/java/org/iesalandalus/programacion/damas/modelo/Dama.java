191b2596f9ca19da7b1c8b4b44227872bc78ab3d
Rafa <rafael.segura.orta@gmail.com>
2024-11-10 14:33:24 +0100

    Creamos Los setters y getters de los atributos de la clase Dama

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -8,17 +8,37 @@ public class Dama {
     private Posicion posicion;
     private static boolean esDamaEspecial;
 
+//Metodos Getters y Setters
 
+    public Color getColor() {
+        return color;
+    }
 
+    public void setColor(Color color) {
+        if (color == null) {
+            throw new IllegalArgumentException("El color no puede ser nulo.");
+        }
+        this.color = color;
+    }
 
+    public Posicion getPosicion() {
+        return posicion;
+    }
 
+    public void setPosicion(Posicion posicion) {
+        if (posicion == null) {
+            throw new IllegalArgumentException("La posición no puede ser nula.");
+        }
+        this.posicion = posicion;
+    }
 
+    public static boolean isEsDamaEspecial() {
+        return esDamaEspecial;
+    }
 
-
-
-
-
-
+    public static void setEsDamaEspecial(boolean esDamaEspecial) {
+        Dama.esDamaEspecial = esDamaEspecial;
+    }
 
 
 
