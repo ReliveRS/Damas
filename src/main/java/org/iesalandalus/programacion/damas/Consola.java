350c2c2fd7a931ebb7d0209aef6e652b43806838
Rafa <rafael.segura.orta@gmail.com>
2024-11-10 14:49:15 +0100

    Creamos metodo menu principal

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -5,7 +5,14 @@ public class Consola {
     private Consola(){
         throw new UnsupportedOperationException("Esta clase no puede ser instanciada.");
     }
-
+// Método para mostrar el menú principal
+    public static void mostrarMenu() {
+        System.out.println("Menú de Opciones:");
+        System.out.println("1. Crear dama por defecto");
+        System.out.println("2. Crear dama eligiendo el color");
+        System.out.println("3. Mover");
+        System.out.println("4. Salir");
+    }
 
 
 
